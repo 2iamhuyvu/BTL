7fcdb91383443fc92b42e7af343f0329c3d59e5c
Cud Lo <loducnt2@gmail.com>
2018-12-15 08:38:30 +0700

    Duc rename again AND Finish my part report

---
+++ BaiTapLonCuoiKhoa/Admin/QFood/ListFood.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Services;
+
+public partial class Admin_QFood_ListFood : System.Web.UI.Page
+{
+    DataUtil data = new DataUtil();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            List<Food> getListMembers = data.getListFood();
+        }
+    }
+
+    [WebMethod]
+    public static string XoaFood(int idFood)
+    {
+        DataUtil data = new DataUtil();
+        data.DeleteFood(idFood);
+        return "Xóa thành công !";
+    }
+
+    [WebMethod]
+    public static List<Food> TimKiem(string keywords)
+    {
+        DataUtil dt = new DataUtil();
+
+        List<Food> li = new List<Food>();
+
+        li = dt.getListFood_Search(keywords);
+
+        return li;
+    }
+}
